badf9c6f9730cb9ce2039f1f1a2c98e67de962f6
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 17:09:06 +0530

    Add new color and modify dimens

--- app/src/main/java/com/compose/jetnote/utils/Dimens.kt
+++ app/src/main/java/com/compose/jetnote/utils/Dimens.kt
@@ -4,7 +4,12 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 enum class Dimens(val value: Dp) {
-    PADDING_9(9.dp),
+    PADDING_4(4.dp),
+    PADDING_6(6.dp),
     PADDING_8(8.dp),
-    PADDING_6(6.dp)
+    PADDING_9(9.dp),
+    PADDING_10(10.dp),
+    PADDING_14(14.dp),
+    ELEVATION_6(6.dp),
+    CORNER_33(33.dp)
 }
