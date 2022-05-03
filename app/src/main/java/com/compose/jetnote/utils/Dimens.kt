cb75f8de569dde25cd59e85f9e045e924c0b3273
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 15:53:59 +0530

    Add enum class for dimension values

---
+++ app/src/main/java/com/compose/jetnote/utils/Dimens.kt
@@ -0,0 +1,10 @@
+package com.compose.jetnote.utils
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Dimens(val value: Dp) {
+    PADDING_9(9.dp),
+    PADDING_8(8.dp),
+    PADDING_6(6.dp)
+}
