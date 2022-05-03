7af9e86014de7e712163863f1d912058fdb0467b
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 15:28:57 +0530

    Add note button component

--- app/src/main/java/com/compose/jetnote/ui/components/NoteComponents.kt
+++ app/src/main/java/com/compose/jetnote/ui/components/NoteComponents.kt
@@ -1,7 +1,9 @@
 package com.compose.jetnote.ui.components
 
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -45,4 +47,21 @@ fun NoteInputText(
         modifier = modifier
     )
 
+}
+
+@Composable
+fun NoteButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        shape = CircleShape,
+        enabled = enabled,
+        modifier = modifier
+    ) {
+        Text(text)
+    }
 }
