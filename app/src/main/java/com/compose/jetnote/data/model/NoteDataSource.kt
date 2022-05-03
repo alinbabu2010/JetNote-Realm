24c3a870459a6aa6de1606b742bc9bd2673df82d
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 17:10:08 +0530

    Add new function to format date and update data source

--- app/src/main/java/com/compose/jetnote/data/model/NoteDataSource.kt
+++ app/src/main/java/com/compose/jetnote/data/model/NoteDataSource.kt
@@ -4,7 +4,7 @@ class NoteDataSource {
 
     fun loadNotes(): List<Note> {
         return listOf(
-            Note(title = "A good day", description = "we wnt on a vacation by the lake"),
+            Note(title = "A good day", description = "We went on a vacation by the lake"),
             Note(title = "Android Compose", description = "Working on Android compose today"),
             Note(title = "Keep at it...", description = "Sometimes things just happen"),
             Note(title = "A movie day", description = "Watching a movie with family"),
--- app/src/main/java/com/compose/jetnote/utils/Utils.kt
+++ app/src/main/java/com/compose/jetnote/utils/Utils.kt
@@ -1,5 +1,14 @@
 package com.compose.jetnote.utils
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+private const val DATE_TIME_FORMAT = "EEE, d MMM"
+
 fun validateInput(input: String): Boolean {
     return input.all { char -> char.isLetter() || char.isWhitespace() }
+}
+
+fun getFormattedDate(dateTime: LocalDateTime): String {
+    return dateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT))
 }
