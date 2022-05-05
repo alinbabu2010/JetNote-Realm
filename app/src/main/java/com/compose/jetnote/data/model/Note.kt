7121ef7bebfb8becd58df45975a255cd1f8e52df
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 16:00:17 +0530

    Modify data class Note by removing Room dependent code

--- app/src/main/java/com/compose/jetnote/data/model/Note.kt
+++ app/src/main/java/com/compose/jetnote/data/model/Note.kt
@@ -1,22 +1,11 @@
 package com.compose.jetnote.data.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.time.Instant
 import java.util.*
 
-@Entity(tableName = "notes_tbl")
 data class Note(
-    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
-
-    @ColumnInfo(name = "note_title")
     val title: String,
-
-    @ColumnInfo(name = "note_description")
     val description: String,
-
-    @ColumnInfo(name = "note_entry_date")
     val entryDate: Date = Date.from(Instant.now())
 )
