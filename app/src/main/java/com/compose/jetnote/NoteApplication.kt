683c42512155c46d038fcb2d442ee1c5bbd7f05c
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 16:36:42 +0530

    Add Realm configuration and initialization in application class

--- app/src/main/java/com/compose/jetnote/NoteApplication.kt
+++ app/src/main/java/com/compose/jetnote/NoteApplication.kt
@@ -2,6 +2,21 @@ package com.compose.jetnote
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 @HiltAndroidApp
-class NoteApplication : Application()
+class NoteApplication : Application() {
+
+    private val realmVersion = 1L
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfiguration = RealmConfiguration.Builder()
+            .schemaVersion(realmVersion)
+            .build()
+        Realm.setDefaultConfiguration(realmConfiguration)
+    }
+
+}
