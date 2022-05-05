0992ad219ccfd18965857dd6364748dde0ca4da1
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 11:46:15 +0530

    Modify utils function getFormattedDate and date format constant

--- app/src/main/java/com/compose/jetnote/utils/Utils.kt
+++ app/src/main/java/com/compose/jetnote/utils/Utils.kt
@@ -1,14 +1,16 @@
 package com.compose.jetnote.utils
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
+import java.util.*
 
-private const val DATE_TIME_FORMAT = "EEE, d MMM"
+private const val DATE_TIME_FORMAT = "EEE, d MMM hh:mm aaa"
 
 fun validateInput(input: String): Boolean {
     return input.all { char -> char.isLetter() || char.isWhitespace() }
 }
 
-fun getFormattedDate(dateTime: LocalDateTime): String {
-    return dateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT))
+fun getFormattedDate(time: Long): String {
+    val date = Date(time)
+    val formatter = SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault())
+    return formatter.format(date)
 }
