46b8cbaefead75fb1ad77a6a1ea17f212048af58
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 11:47:49 +0530

    Add type converters for database

--- app/src/main/java/com/compose/jetnote/data/source/NoteDatabase.kt
+++ app/src/main/java/com/compose/jetnote/data/source/NoteDatabase.kt
@@ -2,9 +2,12 @@ package com.compose.jetnote.data.source
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.compose.jetnote.data.model.Note
+import com.compose.jetnote.utils.DateConverter
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
