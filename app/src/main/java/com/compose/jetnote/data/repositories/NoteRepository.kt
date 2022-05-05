9c5c046bc8f7902a272e5e6f8451f9ed34189dfa
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 10:21:49 +0530

    Add NoteRepository class

---
+++ app/src/main/java/com/compose/jetnote/data/repositories/NoteRepository.kt
@@ -0,0 +1,24 @@
+package com.compose.jetnote.data.repositories
+
+import com.compose.jetnote.data.model.Note
+import com.compose.jetnote.data.source.NoteDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(private val noteDao: NoteDao) {
+
+    suspend fun addNote(note: Note) = noteDao.insert(note)
+
+    suspend fun updateNote(note: Note) = noteDao.update(note)
+
+    suspend fun deleteNote(note: Note) = noteDao.deleteNote(note)
+
+    suspend fun deleteAllNotes() = noteDao.deleteAll()
+
+    suspend fun getNoteById(id: String) = noteDao.getNoteById(id)
+
+    fun getAllNotes(): Flow<List<Note>> = noteDao.getNotes().flowOn(Dispatchers.IO)
+
+}
