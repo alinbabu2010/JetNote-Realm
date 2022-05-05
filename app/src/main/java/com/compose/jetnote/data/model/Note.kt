5bac58b91817f6241853436e7d51a9b5ac55a86f
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 09:28:32 +0530

    Modify Note data class to Room database entity

--- app/src/main/java/com/compose/jetnote/data/model/Note.kt
+++ app/src/main/java/com/compose/jetnote/data/model/Note.kt
@@ -1,11 +1,22 @@
 package com.compose.jetnote.data.model
 
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.util.*
 
+@Entity(tableName = "notes_tbl")
 data class Note(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: Date = Date.from(Instant.now())
 )
