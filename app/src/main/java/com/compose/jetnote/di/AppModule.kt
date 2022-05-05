4bb8920809d41ae91547ea2b3f35195c51f98ae5
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 10:11:40 +0530

    Add providers in AppModule class

--- app/src/main/java/com/compose/jetnote/di/AppModule.kt
+++ app/src/main/java/com/compose/jetnote/di/AppModule.kt
@@ -1,10 +1,32 @@
 package com.compose.jetnote.di
 
+import android.content.Context
+import androidx.room.Room
+import com.compose.jetnote.data.source.NoteDao
+import com.compose.jetnote.data.source.NoteDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao = noteDatabase.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideNoteDatabase(@ApplicationContext context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "notes_db"
+        ).fallbackToDestructiveMigration().build()
+    }
+
 }
