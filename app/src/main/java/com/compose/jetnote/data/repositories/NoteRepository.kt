49505f6f2b15139c316dbd9a6cc376e8ef83dc30
Alin Babu <alinbabu2010@gmail.com>
2022-05-05 16:34:57 +0530

    Modify repository class based on Realm database usage

--- app/src/main/java/com/compose/jetnote/data/repositories/NoteRepository.kt
+++ app/src/main/java/com/compose/jetnote/data/repositories/NoteRepository.kt
@@ -1,18 +1,29 @@
 package com.compose.jetnote.data.repositories
 
-import com.compose.jetnote.data.db.NoteDao
+import com.compose.jetnote.data.db.dao.NoteDao
+import com.compose.jetnote.data.db.entity.NoteEntity
 import com.compose.jetnote.data.model.Note
-import kotlinx.coroutines.Dispatchers
+import com.compose.jetnote.utils.Mapper
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
-class NoteRepository @Inject constructor(private val noteDao: NoteDao) {
+class NoteRepository @Inject constructor(
+    private val noteDao: NoteDao,
+    private val dataToEntityMapper: Mapper<Note, NoteEntity>,
+    private val entityToDataMapper: Mapper<NoteEntity, Note>
+) {
 
-    suspend fun addNote(note: Note) = noteDao.insert(note)
+    suspend fun addNote(note: Note) = noteDao.insert(dataToEntityMapper.map(note))
 
     suspend fun deleteNote(note: Note) = noteDao.deleteNote(note)
 
-    fun getAllNotes(): Flow<List<Note>> = noteDao.getNotes().flowOn(Dispatchers.IO)
+    fun getAllNotes(): Flow<List<Note>> {
+        val noteList = MutableStateFlow(mutableListOf<Note>())
+        noteDao.getNotes().forEach {
+            noteList.value.add(entityToDataMapper.map(it))
+        }
+        return noteList
+    }
 
 }
